048e6b8cbccfba176d5818b0408ecd44bc01b0e4
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-05 11:07:36 -0500

    Fixing pint

--- functions.c
+++ functions.c
@@ -74,7 +74,7 @@ void f_pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		printf("L%u: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else
